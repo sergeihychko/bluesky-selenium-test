751bce11d938a5ef4e2e24ea039cff079909729f
Serge Hychko <shychko@gmail.com>
2026-01-10 12:33:02 -0500

    Creating test for the Timeline form.

--- src/test/java/ui/Timeline.java
+++ src/test/java/ui/Timeline.java
@@ -1,32 +1,30 @@
-package org.example;
+package ui;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import static java.lang.IO.println;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Timeline {
     static void main() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
         //System.setProperty("webdriver.chrome.driver", "C:\\Browser drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-//        driver.get("http://localhost:4200/recent");
-//        driver.manage().window().maximize();
-//        Thread.sleep(6000);
-//        driver.close();
+        driver.get("http://localhost:4200/feed");
+        driver.findElement(By.xpath("//*[@id=\"search-term\"]")).sendKeys("wanted");
+        driver.manage().window().maximize();
+        Thread.sleep(6000);
+        driver.close();
 
-//        System.setProperty("webdriver.gecko.driver", "C:\\Browser drivers\\geckodriver.exe");
 //        FirefoxOptions options = new FirefoxOptions();
 //        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");  // Update path
 //        WebDriver firedriver = new FirefoxDriver(options);
-//        firedriver.get("http://localhost:4200/recent");
+//        firedriver.get("http://localhost:4200/delete");
 //        firedriver.manage().window().maximize();
 //        Thread.sleep(3000);
 //        firedriver.close();
 
-        println("Selenium first new project");
-        }
     }
+}
